220e341700008bf4cab30ee49ba25c59b2fe4820
matvey192 <maivei.gamzin@yandex.ru>
2018-12-04 19:47:52 +0600

    Задача№1

---
+++ Лабораторная 1/Задание 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+---+\n|   |\n|   |\n+---+");
+            Console.WriteLine("#\n##\n###\n####\n#####\n");
+            Console.WriteLine(" /\\ \n//\\\\ \n//\\\\ \n ][");
+            Console.WriteLine("{ \"x\":15, \"y\": 28.7 }");
+            Console.WriteLine(@"Три девицы под окном
+Пряли поздно вечерком.
+""Кабы я была царица,
+Говорит одна девица, —
+То на весь крещеный мир
+Приготовила б я пир""
+""Кабы я была царица, -
+Говорит ее сестрица, —
+То на весь бы мир одна
+Наткала я полотна"".
+""Кабы я была царица, —
+Третья молвила сестрица, —
+Я б для батюшки-царя
+Родила богатыря"".");
+           
+        }
+
+    }
+}
