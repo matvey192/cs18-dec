7af0d4476b0bdd494ca69b2f18f1b70c228ba7f8
matvey192 <maivei.gamzin@yandex.ru>
2019-01-10 20:50:19 +0600

    Лабораторная 7

---
+++ Лабораторная работа 7/Задача 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = Console.ReadLine();
+            string[] sim = b.Split(';');
+            int c = sim.Length / 2;
+            Console.WriteLine("Индекс:"+c+", улемент :"+sim[c]);
+
+        }
+    }
+}
---
+++ Лабораторная работа 7/Задача 7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sim = new string[] { "a", "s", "d", "f", "g", "h" };
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            string c=null;
+            Console.WriteLine("Меняю "+sim[a]+" на "+sim[b]);
+            c = sim[a]; // c=a 
+            sim[a] = sim[b]; // f
+            sim[b] = c; // 
+            Console.WriteLine(String.Join(" ", sim));  
+        }
+    }
+}
