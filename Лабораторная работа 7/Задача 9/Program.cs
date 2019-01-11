c976421b40724a8c5c542626c962ce0460ad096e
mtavey192 <maivei.gamzin@yandex.ru>
2019-01-11 11:32:18 +0600

    Лабораторная работа 7

--- Лабораторная работа 7/Задача 9/Program.cs
+++ Лабораторная работа 7/Задача 9/Program.cs
@@ -4,22 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Задача_8
+namespace Задача_9
 {
     class Program
     {
         static void Main(string[] args)
         {
             string b = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
             string[] sim = b.Split(' ');
-            for (int i = 0; i < 1; ++i)
+            if (a < 1 || a > sim.Length) { Console.WriteLine("Количество позиций при сдвиге должно быть в интервале [1, длина массива)");return; }
+            for (int i = 0; i < a; ++i)
             {
                 string aLast = sim[sim.Length - 1]; // последний элемент массива
                 for (int j = sim.Length - 1; j > 0; j--) // пока не начало массива
                     sim[j] = sim[j - 1];//5 элемент становится 4-м
                 sim[0] = aLast; // по условию до 0 . первый элемент равен последнему 
+                Console.WriteLine(String.Join(" ", sim));
             }
-            Console.WriteLine(String.Join(" ", sim));
         }
     }
 }
