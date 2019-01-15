3cb6ccbaa5a5a0f1ef3d51a808789cd9be25eeb3
mtavey192 <maivei.gamzin@yandex.ru>
2019-01-15 10:49:52 +0600

    ?????

--- Лабораторная работа 7/Задача 35 5/Program.cs
+++ Лабораторная работа 7/Задача 35 5/Program.cs
@@ -20,22 +20,18 @@ namespace Задача_35_5
                 c++;
             }
             c = 0;
-
             int n = num.Length;
-
-            for (int i = 0, m = 0; i != n; i++, n = m)
+            for (int i = 0, m = 0; i != n; i++, n = m) // 
             {
                 for (int j = m = i + 1; j != n; j++)
                 {
-                    if (num[j] != num[i])
+                    if (num[j] != num[i]) // 1 2 
                     {
-                        if (m != j) num[m] = num[j];
+                        if (m != j) num[m] = num[j]; // 
                         m++;
                     }
                 }
             }
-
-
             if (n != num.Length)
             {
                 int[] x = new int[num.Length];
@@ -44,7 +40,13 @@ namespace Задача_35_5
                 num = x;
             }
             int bc = 0;
-            while (bc < num.Length) { Console.WriteLine(num[bc]); bc++; }
+            while (bc < num.Length)
+            {
+                if (num[bc] == 0) { goto brr; }
+                Console.Write(num[bc]);
+            brr:
+                bc++;
+            }
         }
     }
 }
---
+++ Лабораторная работа 7/Задача 37/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = Console.ReadLine();
+            string[] arr = b.Split(';');
+            ShowAllCombinations(arr);
+            Console.Read();
+        }
+        public static void ShowAllCombinations<T>(IList<T> arr, string current = "")
+        {
+            if (arr.Count == 0)
+            {
+                Console.WriteLine(current);
+                return;
+            }
+            for (int i = 0; i < arr.Count; i++) 
+            {
+                List<T> lst = new List<T>(arr);
+                lst.RemoveAt(i);
+                ShowAllCombinations(lst, current + arr[i].ToString());
+            }
+        }
+    }
+}
