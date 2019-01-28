30aafebbf5d827c281673c1268a6d1361b30a106
mtavey192 <maivei.gamzin@yandex.ru>
2019-01-28 13:34:53 +0600

    лр 9

--- Лабораторная 9/Задание 12/Program.cs
+++ Лабораторная 9/Задание 12/Program.cs
@@ -10,6 +10,8 @@ namespace Задание_12
     {
         static void Main(string[] args)
         {
+            int res = 0;
+            int res1 = 0;
             string s = Console.ReadLine();
             string ch = "task4463/test" + s + ".txt";
             if (!File.Exists(ch)) // истино если существует
@@ -19,7 +21,18 @@ namespace Задание_12
             }
             StreamReader sr = new StreamReader(ch);
             if (sr.EndOfStream) { Console.WriteLine("Файл пуст"); sr.Close(); return; }
-
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if(line.Contains("src"))
+                {
+                    res = line.IndexOf('"')+1;
+                    res1 = line.IndexOf('"', res + 1);
+                    line = line.Substring(res,res1-res);
+                    Console.WriteLine(line);
+                }
+                if (sr.EndOfStream && res == 0) { Console.WriteLine("Файл не содержит скриптов"); sr.Close(); return; }
+            }
             sr.Close();
         }
     }
---
+++ Лабораторная 9/Задание 14/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Задание_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            int c = 0;
+            int res = 0;
+            while (s < 11)
+            {
+                
+                string ch = "task9417/data" + s + ".txt";
+                if (!File.Exists(ch)) // истино если существует
+                {
+                    Console.WriteLine("File not found " + Path.GetFullPath(ch));
+                    return;
+                }
+                StreamReader sr = new StreamReader(ch);
+                if (sr.EndOfStream) { Console.WriteLine("Файл пуст"); sr.Close(); return; }
+                while (!sr.EndOfStream)
+                {
+                    int line = int.Parse(sr.ReadLine());
+                    c = line;
+                    res = res + c;
+                    s++;
+                }
+                sr.Close();
+            }
+        }
+    }
+}
