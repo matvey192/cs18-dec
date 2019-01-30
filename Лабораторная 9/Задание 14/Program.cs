ec0a2f10c6be8d700460399e16a750118a899ee4
mtavey192 <maivei.gamzin@yandex.ru>
2019-01-30 12:35:46 +0600

    Лр 9

--- Лабораторная 9/Задание 14/Program.cs
+++ Лабораторная 9/Задание 14/Program.cs
@@ -10,29 +10,40 @@ namespace Задание_14
     {
         static void Main(string[] args)
         {
+            int res1 = 0;
             int s = 0;
             int c = 0;
             int res = 0;
             while (s < 11)
             {
-                
+                brr:
                 string ch = "task9417/data" + s + ".txt";
+                Console.WriteLine("Обработка файла"+ch);
                 if (!File.Exists(ch)) // истино если существует
                 {
-                    Console.WriteLine("File not found " + Path.GetFullPath(ch));
-                    return;
+                    Console.WriteLine("Файл не найден ");
+                    s++;
+                    goto brr;
                 }
                 StreamReader sr = new StreamReader(ch);
-                if (sr.EndOfStream) { Console.WriteLine("Файл пуст"); sr.Close(); return; }
+                if (sr.EndOfStream) { Console.WriteLine("Файл пуст"); sr.Close();s++; goto brr;  }
                 while (!sr.EndOfStream)
                 {
-                    int line = int.Parse(sr.ReadLine());
-                    c = line;
-                    res = res + c;
-                    s++;
+                    try
+                    {
+                        int line = int.Parse(sr.ReadLine());
+                        c = line;
+                        res1 = res1 + c;
+                        res = res + c;
+                    }
+                    catch { Console.WriteLine("Данные некорректного формата");s++;goto brr; }
                 }
-                sr.Close();
+                Console.WriteLine("Сумма в файле = "+res1);
+                res1 = 0;
+                s++;
+                if (s == 10) {  sr.Close(); }
             }
+            Console.WriteLine("Общайя сумма = " + res);
         }
     }
 }
