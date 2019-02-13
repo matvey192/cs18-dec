b3d9ade4f740c7d984ff5eb7fd481ca2e8153441
mtavey192 <maivei.gamzin@yandex.ru>
2019-02-13 13:22:03 +0600

    stage 2 lr 14

--- Stage 2/CodeProject/Point.cs
+++ Stage 2/CodeProject/Point.cs
@@ -8,12 +8,14 @@ namespace CodeProject
 {
     public class Point
     {
+        //1
         public double x;
         public double y;
         public static double distanceBetween(int xa, int ya, int xb, int yb)
         {
             return Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2));
         }
+        //2
         public static double distanceBetween1(Point a,Point b)
         {
             return Math.Sqrt(Math.Pow((b.x-a.x), 2) + Math.Pow((b.y-a.y), 2));
