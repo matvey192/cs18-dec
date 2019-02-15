4a48334401e9fdd991f8e1b8bd94d114f9c95d17
mtavey192 <maivei.gamzin@yandex.ru>
2019-02-15 12:51:51 +0600

    stage 2 lr 14

---
+++ Stage 2/Testing Project/RangeSuite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing_Project
+{
+    [TestClass]
+    public class RangeSuite
+    {
+        [TestMethod]
+        public void HasIntersectionIntsWrongLeftTest()
+        {
+
+        }
+        [TestMethod]
+        public void HasIntersectionIntsWrongRightTest()
+        {
+
+        }
+    }
+}
